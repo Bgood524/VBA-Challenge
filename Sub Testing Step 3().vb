674a4c733aab8c9e28598f8af8e068b28db5825a
Bgood524 <85585524+Bgood524@users.noreply.github.com>
2021-07-07 19:22:20 -0500

    Successful Ticker and Yearly Change

---
+++ Sub Testing Step 3().vb
@@ -0,0 +1,39 @@
+Sub Testing()
+
+Dim TickerCounter As Integer
+Dim TickerList As String
+Dim lastrow As Long
+lastrow = Cells(Rows.Count, 1).End(xlUp).Row
+TickerCounter = 2
+Dim OpenPrice As Double
+Dim ClosePrice As Double
+Dim YearlyChange As Double
+Dim RunningTotal As Long
+ClosePrice = 0
+RunningTotal = 0
+OpenPrice = Cells(2, 3).Value
+
+
+For i = 2 To lastrow
+
+
+If Cells(i + 1, 1) <> Cells(i, 1).Value Then
+   TickerList = Cells(i, 1).Value
+   Range("I" & TickerCounter) = TickerList
+   ClosePrice = ClosePrice + Cells(i, 6).Value
+   RunningTotal = Cells(i, 7).Value + RunningTotal
+   Range("J" & TickerCounter) = ClosePrice - OpenPrice
+
+TickerCounter = TickerCounter + 1
+OpenPrice = Cells(i + 1, 3)
+ClosePrice = 0
+RunningTotal = 0
+
+
+End If
+
+Next i
+
+Cells(1, 10).Value = "Yearly Change"
+
+End Sub
